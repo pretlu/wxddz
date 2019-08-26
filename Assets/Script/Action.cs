4dccb2c2a20c285e6b970242bf33d341ffb821c0
pretlu <javalu@qq.com>
2019-08-26 10:09:47 +0800

    实现各场景的切换

---
+++ Assets/Script/Action.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// 事件函数
+/// </summary>
+public class Action : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject loginPanel;
+    void Start()
+    {
+        loginPanel = GameObject.Find("loginPanel");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /// <summary>
+    /// 显示 隐藏 注册面板
+    /// </summary>
+    /// <param name="game"></param>
+    public void LoginPanelShowHidd()
+    {
+        Debug.Log("show login");
+        UTool.showOrHidd(loginPanel);
+    }
+    /// <summary>
+    /// 进入菜单场景
+    /// </summary>
+    public void LoadSceneMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    /// <summary>
+    /// 进入房间场景
+    /// </summary>
+    public void LoadSceneRoom()
+    {
+        SceneManager.LoadScene("Room");
+    }
+    public void LoadSceneStart()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
---
+++ Assets/Script/UTool.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// unity工具类
+/// </summary>
+public static class UTool
+{
+
+    public static void showUI(GameObject gameObject)
+    {
+        gameObject.GetComponent<CanvasGroup>().alpha = 1;
+        gameObject.GetComponent<CanvasGroup>().interactable = true;
+        gameObject.GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+    public static void hiddUI(GameObject gameObject)
+    {
+        gameObject.GetComponent<CanvasGroup>().alpha = 0;
+        gameObject.GetComponent<CanvasGroup>().interactable = false;
+        gameObject.GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+    /// <summary>
+    /// 显示或隐藏游戏对象
+    /// </summary>
+    /// <param name="game"></param>
+    public static void showOrHidd(GameObject game)
+    {
+        if (game.GetComponent<CanvasGroup>().alpha == 0)
+        {
+            showUI(game);
+        }
+        else
+        {
+            hiddUI(game);
+        }
+    }
+}
